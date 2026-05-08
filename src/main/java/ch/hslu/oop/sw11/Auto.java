225278bdd1c848758bd160e9236c19168dade5b6
chiara.steindl <chiara.steindl@stud.hslu.ch>
2026-05-08 13:59:06 +0200

    sw11 doing

--- src/main/java/ch/hslu/oop/sw11/Auto.java
+++ src/main/java/ch/hslu/oop/sw11/Auto.java
@@ -2,11 +2,31 @@ package ch.hslu.oop.sw11;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+
 public class Auto implements PropertyChangeListener {
+  private final Motor motor;
 
-
-  @Override
-  public void propertyChange(PropertyChangeEvent event){
+  public Auto(){
 
   }
+
+  @Override
+  public void propertyChange(final PropertyChangeEvent event){
+    System.out.println("Motor Status geändert: " + event.getNewValue());
+  }
+
+
+    // if (event.getSource() == this.motor) {
+    //   this.handleMotorEvent("Motor", event);
+    //   System.out.println("Status:", "event.getNewValue()");
+    // }
+
+    // if (event.getSource() == this.lightFrontLeft) {
+    // this.handleLightEvent("Scheinwerfer Links", event); }
+    // if (event.getSource() == this.lightFrontRight) {
+    // this.handleLightEvent("Scheinwerfer Rechts", event); }
+  // }
+
+
+
 }
